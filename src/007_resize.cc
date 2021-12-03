12302721da8e2a22757cd19994397648b3ef0f56
hyt100 <hyt100_code@126.com>
2021-12-04 00:47:37 +0800

    update resize

--- src/007_resize.cc
+++ src/007_resize.cc
@@ -26,7 +26,7 @@ void resize_nearest(cv::Mat &src, cv::Mat &dst, cv::Size dsize)
     }
 }
 
-void resize_linear(cv::Mat &src, cv::Mat &dst, cv::Size dsize)
+void resize_bilinear(cv::Mat &src, cv::Mat &dst, cv::Size dsize)
 {
     cv::Size ssize(src.cols, src.rows);
     dst = cv::Mat(dsize, CV_8UC3);
@@ -66,22 +66,102 @@ void resize_linear(cv::Mat &src, cv::Mat &dst, cv::Size dsize)
     }
 }
 
+// 权重计算公式：
+//              /   (a+2)|x|^3 - (a+3)|x|^2 + 1          |x|<=1
+//      W(x) = |    a|x|^3 - 5a|x|^2 + 8a|x| -4a       1<|x|<2
+//              \   0                                    else
+//      其中a通常取-0.5
+float cal_weight(float x)
+{
+    float a = -0.5f;
+    float x_abs = std::fabs(x);
+    if (x_abs <= 1.0f) {
+        return (float)((a+2)*std::pow(x_abs, 3) - (a+3)*std::pow(x_abs, 2) + 1);
+    } else if (x_abs < 2.0f) {
+        return (float)(a*std::pow(x_abs, 3) - 5*a*std::pow(x_abs, 2) + 8*a*x_abs - 4*a);
+    } else {
+        return 0.0f;
+    }
+}
+
+struct PixelPoint {
+    int x;
+    int y;
+    float weight;
+};
+
+std::vector<PixelPoint> generate16(float x, float y, int width, int height)
+{
+    std::vector<PixelPoint> vec;
+    vec.resize(16);
+
+    int i = (int)std::floor(x) - 1;
+    int j = (int)std::floor(y) - 1;
+
+    //calcurate weight
+    for (int m = 0; m < 16; ++m) {
+        vec[m].x = i + m % 4;
+        vec[m].y = j + m / 4;
+        vec[m].weight = cal_weight(x - vec[m].x) * cal_weight(y - vec[m].y);
+    }
+    //fixed coordinate
+    for (auto &p: vec) {
+        p.x = std::clamp(p.x, 0, width - 1);
+        p.y = std::clamp(p.y, 0, height - 1);
+    }
+
+    return vec;
+}
+
+void resize_bicubic(cv::Mat &src, cv::Mat &dst, cv::Size dsize)
+{
+    cv::Size ssize(src.cols, src.rows);
+    dst = cv::Mat(dsize, CV_8UC3);
+    float scale_x = 1.0 * dsize.width / ssize.width;
+    float scale_y = 1.0 * dsize.height / ssize.height;
+
+    for (int x = 0; x < dsize.width; ++x) {
+        float x0 = x / scale_x;
+        for (int y = 0; y < dsize.height; ++y) {
+            float y0 = y / scale_y;
+            
+            std::vector<PixelPoint> vec = generate16(x0, y0, ssize.width, ssize.height);
+            cv::Vec3f color(0.0f, 0.0f, 0.0f);
+            for (auto &p: vec) {
+                color[0] += p.weight * src.at<cv::Vec3b>(p.y, p.x)[0];
+                color[1] += p.weight * src.at<cv::Vec3b>(p.y, p.x)[1];
+                color[2] += p.weight * src.at<cv::Vec3b>(p.y, p.x)[2];
+            }
+            color[0] = std::clamp(color[0], 0.0f, 255.0f);
+            color[1] = std::clamp(color[1], 0.0f, 255.0f);
+            color[2] = std::clamp(color[2], 0.0f, 255.0f);
+
+            dst.at<cv::Vec3b>(y, x) = cv::Vec3b((uint8_t)color[0], (uint8_t)color[1], (uint8_t)color[2]);
+        }
+    }
+}
+
 int main(int argc, char *argv[])
 {
     cv::Mat origin = cv::imread("../data/lena.jpg");
     int width = origin.cols, height = origin.rows;
     std::cout << "image size: " << width << "x" << height << std::endl;
-    cv::imshow("origin", origin);
+    // cv::imshow("origin", origin);
 
     // nearest
     cv::Mat nearest;
     resize_nearest(origin, nearest, cv::Size(1024, 1024));
     cv::imshow("nearest", nearest);
 
-    // linear
-    cv::Mat linear;
-    resize_linear(origin, linear, cv::Size(1024, 1024));
-    cv::imshow("linear", linear);
+    // bilinear
+    cv::Mat bilinear;
+    resize_bilinear(origin, bilinear, cv::Size(1024, 1024));
+    cv::imshow("bilinear", bilinear);
+
+    // bicubic
+    cv::Mat bicubic;
+    resize_bicubic(origin, bicubic, cv::Size(1024, 1024));
+    cv::imshow("bicubic", bicubic);
 
     cv::waitKey();
     return 0;
